5f47622fe382a046eecfac5a8e6398d121287196
이창준 <naromine@naver.com>
2024-10-23 17:13:21 +0900

    이창준 : PreCompile

--- EnginePlatform/EngineWindow.h
+++ EnginePlatform/EngineWindow.h
@@ -1,5 +1,15 @@
 #pragma once
+// Os Header
 #include <Windows.h>
+
+// std Header
+#include <map>
+#include <string>
+#include <functional>
+
+// user header
+#include <EngineBase/EngineDelegate.h>
+
 
 // 정신 잘 붙
 
@@ -8,8 +18,9 @@ class UEngineWindow
 {
 public:
 	static void EngineWindowInit(HINSTANCE _Instance);
+	static void CreateWindowClass(const WNDCLASSEXA& _Class);
 
-	static int WindowMessageLoop();
+	static int WindowMessageLoop(EngineDelegate _FrameFunction);
 	// constrcuter destructer
 	UEngineWindow();
 	~UEngineWindow();
@@ -20,17 +31,18 @@ public:
 	UEngineWindow& operator=(const UEngineWindow& _Other) = delete;
 	UEngineWindow& operator=(UEngineWindow&& _Other) noexcept = delete;
 
-	void Open();
-
+	void Create(std::string_view _TitleName, std::string_view _ClassName = "Default");
+	void Open(std::string_view _TitleName = "Window");
 
 protected:
 
 private:
 	static HINSTANCE hInstance;
+	static std::map<std::string, WNDCLASSEXA> WindowClasss;
 
 	// 리눅스에서는 컴파일이 안되거나 실행이 안되는 코드가 된다.
 	// hwnd => 위도우 창 1개
 	HWND WindowHandle = nullptr;
-
 };
+
 
